f20c5f0b4aff9b9de1d8374aba4687168798b04b
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-01-11 15:30:40 -0300

    hover effect cards

--- src/components/champs/CardChamp.tsx
+++ src/components/champs/CardChamp.tsx
@@ -6,6 +6,9 @@ import { AppDispatch } from '../../redux/store'
 import { IChamps } from '../../types/champs.types'
 import './CardChamp.scss'
 import { AiOutlineHeart } from 'react-icons/ai'
+import { BsFillArrowRightCircleFill } from 'react-icons/bs'
+import { FaArrowCircleRight } from 'react-icons/fa'
+import { HiArrowCircleRight } from 'react-icons/hi'
 
 function CardChamp({ id, image, title }: IChamps) {
 
@@ -14,34 +17,18 @@ function CardChamp({ id, image, title }: IChamps) {
     return (
         <div key={id} className='card_champ'>
             <Link to='/' className='card_champ_link'>
-
                 <div className='card_champ_img'>
                     <img src={image} alt={image} />
-
-                    <AiOutlineHeart className='fav-icon'/>
+                    {/* <div className="border"></div> */}
+                    <AiOutlineHeart className='fav-icon' />
 
                     <div className='card_champ_info'>
-                        <div className='card_champ_info_name'>{id},</div>
+                        <div className='card_champ_info_name'>{id}</div>
                         <div className='card_champ_info_title'> {title}</div>
                     </div>
-
                 </div>
             </Link>
         </div>
-
-        // <li className='cards__item'>
-        //     <Link className='cards__item__link' to='/home'  >
-        //         <figure className='cards__item__pic-wrap'>
-        //             <img src={image} alt={image} />
-
-        //         </figure>
-
-        //         <div className='cards__item__info'>
-        //             <h5 className='cards__item__text'>ll</h5>
-        //         </div>
-
-        //     </Link>
-        // </li>
     )
 }
 
