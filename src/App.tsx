48a64fb0e91b0c34870082af4c3bf6a4834bc0ac
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-01-30 21:00:54 -0300

    mui navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import ResponsiveAppBar from './components/navbar/ResponsiveAppBar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <ResponsiveAppBar />
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/favorites" element={<Home />} /> */}
