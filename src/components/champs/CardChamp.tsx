cbf001bcbebddceb4d769d6cd24db421046d6a07
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-02-23 23:01:12 -0300

    fav button

--- src/components/champs/CardChamp.tsx
+++ src/components/champs/CardChamp.tsx
@@ -8,61 +8,38 @@ import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 import Tooltip from '@mui/material/Tooltip';
 import { splitName } from 'helpers/splitName'
 import { addFavorite, removeFavorite } from 'redux/states'
+import FavButton from './FavButton'
 
 /**
  * Component that display the card with the information of the champion
  */
 function CardChamp({ id, image, title, tags }: IChamps): JSX.Element {
-
-    const dispatch = useDispatch();
-    const favorites = useSelector((store: AppStore) => store.favorites);
-    const champs = useSelector((store: AppStore) => store.champs)
-
-    const findFav = favorites?.find(fav => fav.id === id);
-    const filterPerson = champs?.find(p => p.id === id);
-
-    function manageFavorites() {
-        if (findFav) {
-            dispatch(removeFavorite(filterPerson))
-        } else {
-            dispatch(addFavorite(filterPerson))
-        }
-    }
-
     return (
         <li key={id} className='card_champ'>
-                <div className='card_champ_img'>
-            <Link to='/asd' className='card_champ_link'>
+            <div className='card_champ_img'>
+                <Link to={`/${id}`} className='card_champ_link'>
                     <img src={image} alt={image} />
-                    </Link>
-                    <button
-                        aria-label="add to favorites"
-                        onClick={() => manageFavorites()}
-                    >
-                        {!findFav
-                            ? <AiOutlineHeart className='fav-icon' />
-                            : <AiFillHeart className='fav-icon' />
-                        }
-                    </button>
-                    <div className='card_champ_info'>
-                        <div className='card_champ_info_name'>{splitName(id)}</div>
-                        <div className='card_champ_info_title'> {title}</div>
-                        <div className='card_champ_info_tags'>
-                            {tags.map(tag => {
-                                return (
-                                    <Tooltip title={`${tag}`} arrow key={`${tag}`} >
-                                        <img
-                                            src={`/tags/${tag}.png`}
-                                            alt={`${id + tag}`}
-                                            key={`${id + tag}`}
-                                        // title={`${tag}`}
-                                        />
-                                    </Tooltip>
-                                )
-                            })}
-                        </div>
+                </Link>
+                <FavButton id={id} />
+                <div className='card_champ_info'>
+                    <div className='card_champ_info_name'>{splitName(id)}</div>
+                    <div className='card_champ_info_title'> {title}</div>
+                    <div className='card_champ_info_tags'>
+                        {tags.map(tag => {
+                            return (
+                                <Tooltip title={`${tag}`} arrow key={`${tag}`} >
+                                    <img
+                                        src={`/tags/${tag}.png`}
+                                        alt={`${id + tag}`}
+                                        key={`${id + tag}`}
+                                    // title={`${tag}`}
+                                    />
+                                </Tooltip>
+                            )
+                        })}
                     </div>
                 </div>
+            </div>
         </li>
     )
 }
--- src/components/champs/FavButton.tsx
+++ src/components/champs/FavButton.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
-import { AppDispatch, AppStore } from 'redux/store';
+import { AppStore } from 'redux/store';
 import { useDispatch, useSelector } from 'react-redux';
 import { addFavorite, removeFavorite } from 'redux/states';
 
 
 function FavButton({ id }: { id: string }) {
-    const dispatch = useDispatch<AppDispatch>();
-    const favorites = useSelector((state: AppStore) => state.favorites)
-    const esFavorito = favorites.find(fav => fav.id === id);
+    const dispatch = useDispatch();
+    const favorites = useSelector((store: AppStore) => store.favorites);
+    const champs = useSelector((store: AppStore) => store.champs)
 
-    function manageFavorites() {      
-        // return !esFavorito
-        //     ? dispatch(addFavorite(id))
-        //     : dispatch(removeFavorite(id))
-    };
+    const findFav = favorites?.find(fav => fav.id === id);
+    const filterPerson = champs?.find(p => p.id === id);
+
+    function manageFavorites() {
+        findFav
+            ? dispatch(removeFavorite(filterPerson))
+            : dispatch(addFavorite(filterPerson))
+    }
 
     return (
-        <button aria-label="add to favorites" onClick={() => manageFavorites()}>
-            {!esFavorito
+        <button
+            aria-label="add to favorites"
+            onClick={() => manageFavorites()}
+        >
+            {!findFav
                 ? <AiOutlineHeart className='fav-icon' />
                 : <AiFillHeart className='fav-icon' />
             }
