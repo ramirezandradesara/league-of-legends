8f44709e482d34f25ce334b9fa199ad3decbb2ba
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-02-28 11:07:55 -0300

    no favs styles

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -1,30 +1,36 @@
 import CardChamp from 'components/champs/CardChamp'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { AppStore } from 'redux/store'
+import 'styles/Favorites.scss'
 
 function Favorites() {
-
   const favorites = useSelector((state: AppStore) => state.favorites)
-
-  /**
-   * When the components renders, it gets the champions of the API and set them in the champs state
-   */
 
   return (
     <div className='home'>
-      <ul className="champs_grid">
-        {favorites.map(c => {
-          return (
-            <CardChamp
-              key={c.id}
-              id={c.id}
-              image={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c.id}_0.jpg`}
-              title={c.title}
-              tags={c.tags}
-            />
-          )
-        })}
-      </ul>
+      {favorites.length === 0
+        ? <div className='no_fav_container'>
+          <img src='/tags/poro.png' alt="no favorites" />
+          <h2>One of them must be your favorite</h2>
+          <p>
+            Let's search them in the <Link to='/'>home page</Link>.
+          </p>
+        </div>
+        : <ul className="champs_grid">
+          {favorites.map(c => {
+            return (
+              <CardChamp
+                key={c.id}
+                id={c.id}
+                image={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c.id}_0.jpg`}
+                title={c.title}
+                tags={c.tags}
+              />
+            )
+          })}
+        </ul>
+      }
     </div>
   )
 }
