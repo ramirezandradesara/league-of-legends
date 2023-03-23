274b38dd0d203f8bc414375d1143068c81486840
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-03-23 16:54:35 -0300

    update APIs

--- src/helpers/apis.ts
+++ src/helpers/apis.ts
@@ -1,5 +1,5 @@
-export const allChampsApi = 'https://ddragon.leagueoflegends.com/cdn/13.6.1//data/en_US/champion.json';
+export const allChampsApi = 'https://ddragon.leagueoflegends.com/cdn/13.6.1/data/en_US/champion.json';
 export const passiveImage = 'http://ddragon.leagueoflegends.com/cdn/13.6.1/img/passive/';
 // export const spellsImage = 'https://ddragon.leagueoflegends.com/cdn/7.24.2/img/spell/';
-export const spellsImage = 'http://ddragon.leagueoflegends.com/cdn/12.14.1/img/spell/';
+export const spellsImage = 'http://ddragon.leagueoflegends.com/cdn/13.6.1/img/spell/';
 export const horizontalImageChamp = 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/'
