a635a326d773d4954bab6d7a7d36ab1bc38842be
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-05-01 12:11:56 -0300

    focus styles on search input

--- src/components/champs/ChampsFilter.tsx
+++ src/components/champs/ChampsFilter.tsx
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux';
 import { setChamps } from 'redux/states';
 import { AppDispatch } from 'redux/store';
 import { getChamps } from 'service/getAllChamps'
-import { styled } from '@mui/material/styles';
+import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
 import InputAdornment from '@mui/material/InputAdornment';
 import OutlinedInput from '@mui/material/OutlinedInput';
@@ -20,17 +20,18 @@ export function ChampsFilter() {
   const [input, setInput] = React.useState('')
 
   React.useEffect(() => {
+    inputRef.current.focus();
     getChamps().then(data => setChampsByFilter(Object.values(data)));
   }, [])
 
   React.useEffect(() => {
     const filterByTag = () => {
       if (filter === 'All') {
-        const newChamps = champsByFilter.filter((champ: any) => champ.id.toLowerCase().includes(input.toLowerCase()))
+        const newChamps = champsByFilter.filter((champ: any) => champ.id.toLowerCase().startsWith(input.toLowerCase()))
         return dispatch(setChamps(newChamps))
       } else {
         const newChamps = champsByFilter.filter((champ: any) => champ.tags.includes(filter)
-          && champ.id.toLowerCase().includes(input.toLowerCase()))
+          && champ.id.toLowerCase().startsWith(input.toLowerCase()))
         return dispatch(setChamps(newChamps))
       }
     };
@@ -48,20 +49,35 @@ export function ChampsFilter() {
     },
   });
 
+  const inputRef = React.useRef(null);
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#6759e8',
+      }
+    },
+  });
+
+
   return (
     <Box sx={{ marginBottom: '35px', width: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
       <Box sx={{ display: 'flex', flexWrap: "wrap", alignItems: 'flex-end', justifyContent: 'center', marginBottom: '20px', width: '100%', }}>
-        <OutlinedInput
-          onChange={(e) => setInput(e?.target?.value)}
-          placeholder='Search champion'
-          type='text'
-          sx={{ color: 'white', fontFamily: "bold-lol", border: '1px solid white', width: { md: '50%', xs: '80%' } }}
-          startAdornment={
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          }
-        />
+        <ThemeProvider theme={theme}>
+          <OutlinedInput
+            color='primary'
+            inputRef={inputRef}
+            onChange={(e) => setInput(e?.target?.value)}
+            placeholder='Search champion'
+            type='text'
+            sx={{ color: 'white', fontFamily: "bold-lol", border: '1.5px solid #6759e8', width: { md: '50%', xs: '80%' } }}
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            }
+          />
+        </ThemeProvider>
       </Box>
 
       <StyledButtonGroup variant="text" aria-label="text button group" size="large"
