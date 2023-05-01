268abe7b4853a96df2922a0e47006426b3a3e599
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-05-01 01:08:27 -0300

    styles search input

--- src/components/champs/ChampsFilter.tsx
+++ src/components/champs/ChampsFilter.tsx
@@ -7,7 +7,6 @@ import { setChamps } from 'redux/states';
 import { AppDispatch } from 'redux/store';
 import { getChamps } from 'service/getAllChamps'
 import { styled } from '@mui/material/styles';
-import { TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import InputAdornment from '@mui/material/InputAdornment';
 import OutlinedInput from '@mui/material/OutlinedInput';
@@ -17,38 +16,49 @@ const tags = ["All", "Support", "Tank", "Fighter", "Mage", "Assassin", "Marksman
 export function ChampsFilter() {
   const dispatch = useDispatch<AppDispatch>();
   const [champsByFilter, setChampsByFilter] = React.useState([])
-
-  function filterByTag(tag: string) {
-    if (tag === 'All') {
-      return dispatch(setChamps(champsByFilter))
-    } else {
-      const newChamps = champsByFilter.filter((champ: any) => champ.tags.includes(tag))
-      return dispatch(setChamps(newChamps))
-    }
-  };
+  const [filter, setFilter] = React.useState('All')
+  const [input, setInput] = React.useState('')
 
   React.useEffect(() => {
-    getChamps().then(data => setChampsByFilter(Object.values(data)))
+    getChamps().then(data => setChampsByFilter(Object.values(data)));
   }, [])
+
+  React.useEffect(() => {
+    const filterByTag = () => {
+      if (filter === 'All') {
+        const newChamps = champsByFilter.filter((champ: any) => champ.id.toLowerCase().includes(input.toLowerCase()))
+        return dispatch(setChamps(newChamps))
+      } else {
+        const newChamps = champsByFilter.filter((champ: any) => champ.tags.includes(filter)
+          && champ.id.toLowerCase().includes(input.toLowerCase()))
+        return dispatch(setChamps(newChamps))
+      }
+    };
+    filterByTag()
+  }, [champsByFilter, dispatch, filter, input])
+
 
   const StyledButtonGroup = styled(ButtonGroup)({
     // change the button group dividers color
     '& .MuiButtonGroup-grouped:not(:last-of-type)': {
-      borderColor: "white"
-    }
+      borderRight: '1.5px solid #6759e8'
+    },
+    '& .css-m97oia-MuiButtonBase-root-MuiButton-root:hover': {
+      textDecoration: 'inherent'
+    },
   });
 
   return (
     <Box sx={{ marginBottom: '35px', width: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-      <Box sx={{ display: 'flex', flexWrap: "wrap",alignItems: 'flex-end', justifyContent: 'center', marginBottom: '20px', width: '100%', }}>
+      <Box sx={{ display: 'flex', flexWrap: "wrap", alignItems: 'flex-end', justifyContent: 'center', marginBottom: '20px', width: '100%', }}>
         <OutlinedInput
-          id="outlined-adornment-password"
-          placeholder='Search champion...'
+          onChange={(e) => setInput(e?.target?.value)}
+          placeholder='Search champion'
           type='text'
           sx={{ color: 'white', fontFamily: "bold-lol", border: '1px solid white', width: { md: '50%', xs: '80%' } }}
           startAdornment={
             <InputAdornment position="start">
-              <SearchIcon style={{ color: '#000' }} />
+              <SearchIcon />
             </InputAdornment>
           }
         />
@@ -62,11 +72,13 @@ export function ChampsFilter() {
         }}
       >
         {tags.map(tag => (
-          <Button onClick={() => filterByTag(tag)}
+          <Button onClick={() => setFilter(tag)}
             key={tag} sx={{
               color: '#fff',
-              borderColor: '#fff',
               fontFamily: "bold-lol",
+              textDecoration: filter === tag && "underline solid #6759e8",
+              textUnderlineOffset: '5px',
+              textDecorationThickness: '3px'
             }}>
             {tag}
           </Button>
