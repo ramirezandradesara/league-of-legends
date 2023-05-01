ba5992a540101cb764f265e5ff7bd645d8bb0ea5
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-05-01 12:48:11 -0300

    no results gif

--- src/components/champs/ChampsGrid.tsx
+++ src/components/champs/ChampsGrid.tsx
@@ -8,19 +8,29 @@ function ChampsGrid(): JSX.Element {
   const champs = useSelector((state: AppStore) => state.champs)
 
   return (
-    <ul className="champs_grid">
-      {champs.map(c => {
-        return (
-          <CardChamp
-            key={c.id}
-            id={c.id}
-            image={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c.id}_0.jpg`}
-            title={c.title}
-            tags={c.tags}
-          />
-        )
-      })}
-    </ul>
+    <>
+      {champs.length === 0
+        ? <div className='no_fav_container'>
+          <img src="/gifs/blitzcrank.gif" height={200} alt="no results gif" />
+          <h2>No matches found</h2>
+        </div>
+        : <>
+          <ul className="champs_grid">
+            {champs.map(c => {
+              return (
+                <CardChamp
+                  key={c.id}
+                  id={c.id}
+                  image={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c.id}_0.jpg`}
+                  title={c.title}
+                  tags={c.tags}
+                />
+              )
+            })}
+          </ul>
+        </>
+      }
+    </>
   )
 }
 
