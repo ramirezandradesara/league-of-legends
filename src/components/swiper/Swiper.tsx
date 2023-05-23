f918869f89a36a2a7825230a66d18389bda69108
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-05-23 12:15:37 -0300

    infinite loop swiper

--- src/components/swiper/Swiper.tsx
+++ src/components/swiper/Swiper.tsx
@@ -19,10 +19,12 @@ function SwiperSkins({ skins, champion }: { skins: any, champion: string }) {
                 parallax={true}
                 spaceBetween={30}
                 effect={"fade"}
+                slidesPerView={1}
+                loop={true}
                 autoplay={{
                     delay: 2500,
                     disableOnInteraction: false,
-                  }}
+                }}
                 navigation={true}
                 pagination={{
                     clickable: true,
